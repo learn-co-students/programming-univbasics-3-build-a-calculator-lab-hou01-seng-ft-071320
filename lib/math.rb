0da3d6b9915a3c7d60051bd0fe4d0ac35246b216
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-02 11:22:05 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,26 @@
 def addition(num1, num2)
-
+  math=num1 + num2
+  return math
 end
 
 def subtraction(num1, num2)
-
+  math=num1 - num2
+  return math
 end
 
 def division(num1, num2)
-
+  math=num1 / num2
+  return math
 end
 
 def multiplication(num1, num2)
-
+  math=num1 * num2
 end
 
 def modulo(num1, num2)
-
+  math=num1 % num2
 end
 
 def square_root(num)
-
+  math=Math.sqrt(num)
 end
